8cdb0c9315df82b0c29510eaada0d5ca487f928f
Juhyeon Lee <81895590+gogori6565@users.noreply.github.com>
2026-02-11 17:21:40 +0900

    Update KakaoOAuthService.java

--- src/main/java/com/moongeul/backend/api/member/service/KakaoOAuthService.java
+++ src/main/java/com/moongeul/backend/api/member/service/KakaoOAuthService.java
@@ -30,9 +30,9 @@ public class KakaoOAuthService {
     @Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
     private String clientSecret;
 
-    @Value("${oauth-config.google.local}")
+    @Value("${oauth-config.kakao.local}")
     private String localRedirectUri;
-    @Value("${oauth-config.google.deploy}")
+    @Value("${oauth-config.kakao.deploy}")
     private String deployRedirectUri;
 
     // Kakao 토큰 획득 로직
